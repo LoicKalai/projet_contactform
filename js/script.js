50d92050fafeefa2315928ea91c56551a5a7e5f8
Loïc Kalai <lkalaidjoglou@PORT202109-050.localdomain>
2022-02-11 15:59:39 +0100

    [Ajout]
    Début de Js et ajout pattern

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
- 
+let mail = document.querySelector('.inp3');
+let errormail = document.querySelector('.errormail')
+let mailval = mail.value
+let regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+if (regex.test(String(mailval))== false) {
+    errormail.newContent = "Veuillez entrer un mail valide"
     
-    
+}
