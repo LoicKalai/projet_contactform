4868104381a61955747c7c19702b67c3c29aefba
Loïc Kalai <lkalaidjoglou@PORT202109-050.localdomain>
2022-02-15 11:16:37 +0100

    [Essaie]
    Essaie d'ajout phrase d'erreur

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-
 let tel = document.getElementsByClassName('.tel');
-let mail = ducment.getElementsByClassName('.mail');
+let mail = document.getElementsByClassName('.mail');
 let erreur = document.getElementsByClassName('.erreur')
+let button = document.querySelector('button')
+
+
+    button.addEventListener('click', function(){})
+    
+
+      
+    
